c64d3fd6c6728c2d992850d6d7b1ceef3b1dac06
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-24 16:26:51 +0300

    updated: firebase realtime database - data structure analysis

---
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsfirebaserealtimedatabase/datamodel/Persons.kt
@@ -0,0 +1,9 @@
+package com.ismailmesutmujde.kotlininternetbasedtransactionsfirebaserealtimedatabase.datamodel
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Persons (var person_name:String? = "",
+                    var person_age: Int? = 0) {
+
+}
--- app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsfirebaserealtimedatabase/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsfirebaserealtimedatabase/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ismailmesutmujde.kotlininternetbasedtransactionsfirebaserealtimedatabase
+package com.ismailmesutmujde.kotlininternetbasedtransactionsfirebaserealtimedatabase.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlininternetbasedtransactionsfirebaserealtimedatabase.R
 
 
 class MainActivity : AppCompatActivity() {
